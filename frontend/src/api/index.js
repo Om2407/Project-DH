4d8ad530ee20e91601565b53ab6134487d4b4e61
Om Gupta <your_email@example.com>
2026-04-04 06:21:47 +0530

    err detect

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://project-1xcrtglyh-decodechn-1922s-projects.vercel.app/api',
+  baseURL: 'https://project-dh-steel.vercel.app/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
